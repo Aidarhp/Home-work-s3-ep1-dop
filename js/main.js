711dcca45e720c9cdaaf2eb54b85ef80f09a65f3
Aidar satubaldiev <aydatsatubaldiev@gmail.com>
2022-10-11 12:18:03 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function count(a,b,s,d,f,g) {
+    return a + b + s + d + f + g;
+}
+let result = [count(2,3,4,5,6,7)]
+let second_result = count(2,3,4,5,6,7)
+console.log(result);
+console.log(second_result);
